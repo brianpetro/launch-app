f0c554a4f0044b0df4d33cb28bec96be288167f6
Brian Petro <brian@plexm.com>
2015-03-26 10:57:29 -0400

    generate new mailer

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,14 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/confirm
+  def confirm
+    UserMailer.confirm
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
+  def welcome
+    UserMailer.welcome
+  end
+
+end
