52ce4faf96cd2b3af72a4b5ad54b826afe9922e8
Brian Petro <brian@plexm.com>
2015-03-27 13:36:01 -0400

    remove msg parameter after adding alert box for cleaner feel

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,14 +14,40 @@ var i_tracker = getParameterByName('i');
 
 // display messages on cached pages via URL 'msg' parameter
 var url_msg = getParameterByName('msg');
+
+// remove URL from parameter
+// ref: http://stackoverflow.com/a/1634841/1885896
+var removeUrlParameter = function (url, parameter) {
+  var urlparts= url.split('?');   
+  if (urlparts.length>=2) {
+    var prefix= encodeURIComponent(parameter)+'=';
+    var pars= urlparts[1].split(/[&;]/g);
+    for (var i= pars.length; i-- > 0;) { 
+      if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
+        pars.splice(i, 1);
+      }
+    }
+    url= urlparts[0]+'?'+pars.join('&');
+    return url;
+  } else {
+    return url;
+  }
+}
+
 $(function(){
   // update hidden input if tracker value present
   if ((i_tracker.length > 0) && (i_tracker.length < 200)){
     $('#user_tracker').val(i_tracker);
   }
 
-  // if URL param 'msg' is present, show in primary panel
+  // if URL param 'msg' is present...
   if (url_msg.length > 0){
+    // inject text(param value) into alert box on primary panel
     $('.panel-primary .panel-body .alert').text(url_msg).show();
+
+    // remove 'msg' parameter for cleaner presentation
+    var clean_url = removeUrlParameter(location.href, 'msg');
+    history.replaceState({}, '', clean_url);
   }
+
 })
