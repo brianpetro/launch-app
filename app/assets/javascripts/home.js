dcb36a3da6638795621291db2e02da2428fed60b
Brian Petro <brian@plexm.com>
2015-03-27 12:54:19 -0400

    build users#create action, use strong params. Notify user of form submission status via URL params(instead of ERB default) anticipating CDN delivery.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,15 +4,24 @@ var getParameterByName = function (name) {
   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
   results = regex.exec(location.search);
+  // returns empty string if parameter is not present
   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 // default tracking uses 'i' parameter to set user_tracker
 // set var i_tracker to value of 'i' parameter
 var i_tracker = getParameterByName('i');
+
+// display messages on cached pages via URL 'msg' parameter
+var url_msg = getParameterByName('msg');
 $(function(){
   // update hidden input if tracker value present
   if ((i_tracker.length > 0) && (i_tracker.length < 200)){
     $('#user_tracker').val(i_tracker);
+  }
+
+  // if URL param 'msg' is present, show in primary panel
+  if (url_msg.length > 0){
+    $('.panel-primary .panel-body .alert').text(url_msg).show();
   }
 })
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
+  # POST /users
+  # POST /users.json
   def create
-    render json: {}.to_json, status: 200
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path(msg: 'Confirm your email to complete request.') }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { redirect_to root_path(msg: 'Unable to complete request.') }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
   def confirm
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :tracker)
+    end
 end
