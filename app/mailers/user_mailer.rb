6b2e6eacf83dfcc760a5477c805df112ed540642
Brian Petro <brian@plexm.com>
2015-03-29 13:19:38 -0400

    Reverse order of initial emails to stop using email as confirm access token, replaced with ID.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,23 +1,16 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
+  # Subjects can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.confirm.subject
+  #   en.user_mailer.welcome.subject
   #
   def confirm(user)
     @user = user
-    mail to: user.email, from: ENV['FROM_EMAIL'], subject: 'Confirm your email to complete request'
+    mail to: user.email, from: ENV['FROM_EMAIL']
   end
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.welcome.subject
-  #
-  def welcome
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def welcome(user)
+    @user = user
+    mail to: user.email, from: ENV['FROM_EMAIL']
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :email, format: /@/
   validates_uniqueness_of :email
-  before_create :send_confirmation 
+  before_create :send_welcome
+  after_create :send_confirmation 
 
   # Token system based on Active Support Message Verifier
   # ref: http://ngauthier.com/2013/01/rails-unsubscribe-with-active-support-message-verifier.html
@@ -13,20 +14,23 @@ class User < ActiveRecord::Base
     ActiveSupport::MessageVerifier.new(Rails.application.secrets[:secret_key_base])
   end
   def self.read_access_token(signature)
-    email = verifier.verify(signature)
-    User.where(email: email).last
+    id = verifier.verify(signature)
+    User.find id
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     nil
   end
   def self.create_access_token(user)
-    verifier.generate(user.email)
+    verifier.generate(user.id)
   end
 
   private
     def send_confirmation
       UserMailer.confirm(self).deliver_now!
+    end
+    def send_welcome
+      UserMailer.welcome(self).deliver_now!
     rescue Net::SMTPServerBusy
-      self.errors.add(:email, 'invalid. Unable to send confirmation to that address.')
+      self.errors.add(:email, 'invalid. Unable to send emails to that address.')
       false
     end
 
