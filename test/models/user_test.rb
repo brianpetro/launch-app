4e07a76d612487b944ac6272e5f3e346fabe0f1d
Brian Petro <brian@plexm.com>
2015-04-07 15:07:03 -0400

    test access token for confirmation email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User.access_token stores data used to retrieve User object via read_access_token" do
+    user = users(:two)
+    signature = user.access_token
+    assert_equal User.read_access_token(signature), user
+  end
 end
